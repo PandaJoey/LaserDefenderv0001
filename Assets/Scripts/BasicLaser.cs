0ee371f577c6128887b71b123bc3f253d03e59b9
Joe Crickmore <up623013@myport.ac.uk>
2018-02-20 13:00:19 +0000

    20/02/18

--- Assets/Scripts/BasicLaser.cs
+++ Assets/Scripts/BasicLaser.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicLaser : MonoBehaviour {
-
+	public float damage = 1.0f;
 	// Use this for initialization
 	void Start () {
 		
---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		BasicLaser laser = collider.gameObject.getComponent<Projectile>();
+		if(laser) {
+			Debug.Log("Hit By a Laser");
+		}
+		
+	}
+
+
+}
